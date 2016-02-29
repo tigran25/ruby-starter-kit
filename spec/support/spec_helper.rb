ef1f1d7be4667b86ca694d887ab7ae8d099bf7d6
tigran25 <tigranvetisyan@gmail.com>
2016-03-01 01:08:47 +0300

    chore: coverage

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -1,8 +1,9 @@
 require 'codeclimate-test-reporter'
 require 'webmock/rspec'
+CodeClimate::TestReporter.start
+
 require_relative '../../lib/environment'
 
-CodeClimate::TestReporter.start
 Dir["#{Environment::APP_ROOT}/spec/support/**/*.rb"].each { |f| require f }
 
 WebMock.disable_net_connect!(allow_localhost: true, allow: ['api.codacy.com', 'codeclimate.com'])
