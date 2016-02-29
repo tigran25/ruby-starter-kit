bcbe9fd7a34b90aa148eeb3dd30ed63834f4251c
tigran25 <tigranvetisyan@gmail.com>
2016-02-29 23:52:31 +0300

    chore: handle missing requird arg with usage info

--- cbr_rate.rb
+++ cbr_rate.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
 require_relative 'lib/environment'
 
+unless ENV['date']
+  fail 'Usage: date="2015-09-16" code=BRL ./cbr_rate.rb'
+end
+
 puts CbrRatesProvider.new(ENV['date']).get(ENV['code'] || 'USD')
